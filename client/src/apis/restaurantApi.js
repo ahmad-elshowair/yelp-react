6a5ae83c9606fa7995835d45dfe51568469a04cf
Ahmad Elshowair <it.is.work@outlook.com>
2022-08-29 14:45:28 +0700

    edit api link

--- client/src/apis/restaurantApi.js
+++ client/src/apis/restaurantApi.js
@@ -2,5 +2,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://ahmad-yelp-app.herokuapp.com/restaurants'
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://ahmad-yelp-app.herokuapp.com/restaurants'
 });
