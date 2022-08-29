5a9ac62e9fbd8ed617d48a5022556604ddf449cc
Ahmad Elshowair <it.is.work@outlook.com>
2022-08-29 15:51:18 +0700

    declare variables for api

--- client/src/apis/restaurantApi.js
+++ client/src/apis/restaurantApi.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
 
+const deployApi= "https://ahmad-yelp-app.herokuapp.com",
+  devApi = 'http://localhost:3005';
 export default axios.create({
-  baseURL: 'https://ahmad-yelp-app.herokuapp.com'
+  baseURL: devApi
 });
