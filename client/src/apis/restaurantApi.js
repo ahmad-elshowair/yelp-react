ceb853938d4090fbbc8f18b3ba8666ef05ee1e9d
Ahmad Elshowair <it.is.work@outlook.com>
2022-08-29 17:09:29 +0700

    modify the productive api

--- client/src/apis/restaurantApi.js
+++ client/src/apis/restaurantApi.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const deployApi= "https://ahmad-yelp-app.herokuapp.com",
+const deployApi= "https://ahmad-yelp-app.herokuapp.com/restaurantts",
   devApi = "http://localhost:3005/restaurants";
 export default axios.create({
   baseURL: devApi
